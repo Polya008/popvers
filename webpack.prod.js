e550ad117ecb53a09ec125d3a8af405942e7c8ef
Polya008 <polskaya007@mail.ru>
2023-09-12 20:20:29 +0300

    version 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
   stats: {
     children: true,
-    },
+  },
   optimization: {
     minimize: true,
     minimizer: [
